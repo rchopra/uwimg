e7d238296e8f3ea97eb82ce59087ae6a8f676c7c
Rishi Chopra <rqchopra@gmail.com>
2022-05-12 21:56:52 -0500

    HW 0.1: Copying images

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -32,10 +32,9 @@ void set_pixel(image im, int x, int y, int c, float v) {
   im.data[index] = v;
 }
 
-image copy_image(image im)
-{
+image copy_image(image im) {
     image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
+    memcpy(copy.data, im.data, sizeof(float) * (im.w * im.h * im.c));
     return copy;
 }
 
