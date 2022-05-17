1a65f9b0f9a3b4206708583a512e49b9d815875a
Rishi Chopra <rqchopra@gmail.com>
2022-05-16 21:08:16 -0500

    HW 1.4: Bilinear Resizing

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -46,9 +46,21 @@ float bilinear_interpolate(image im, float x, float y, int c) {
     return q;
 }
 
-image bilinear_resize(image im, int w, int h)
-{
-    // TODO
-    return make_image(1,1,1);
+image bilinear_resize(image im, int w, int h) {
+    float aw = (float)im.w / w;
+    float ah = (float)im.h / h;
+    float bw = 0.5 * (aw - 1);
+    float bh = 0.5 * (ah - 1);
+
+    image img = make_image(w, h, im.c);
+    for (int i = 0; i < w; i++) {
+      for (int j = 0; j < h; j++) {
+        for (int k = 0; k < im.c; k++) {
+          float val = bilinear_interpolate(im, aw * i + bw, ah * j + bh, k);
+          set_pixel(img, i, j, k, val);
+        }
+      }
+    }
+    return img;
 }
 
--- tryhw1.py
+++ tryhw1.py
@@ -2,3 +2,13 @@ from uwimg import *
 im = load_image("data/dogsmall.jpg")
 a = nn_resize(im, im.w*4, im.h*4)
 save_image(a, "dog4x-nn")
+
+from uwimg import *
+im = load_image("data/dogsmall.jpg")
+a = bilinear_resize(im, im.w*4, im.h*4)
+save_image(a, "dog4x-bl")
+
+from uwimg import *
+im = load_image("data/dog.jpg")
+a = nn_resize(im, im.w//7, im.h//7)
+save_image(a, "dog7th-bl")
