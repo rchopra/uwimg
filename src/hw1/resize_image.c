06a397470b843b58a7b6478b760610b4032ff32f
Rishi Chopra <rqchopra@gmail.com>
2022-05-16 20:29:42 -0500

    HW 1.2: Nearest Neighbor Resizing

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -5,10 +5,22 @@ float nn_interpolate(image im, float x, float y, int c) {
     return get_pixel(im, round(x), round(y), c);
 }
 
-image nn_resize(image im, int w, int h)
-{
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+image nn_resize(image im, int w, int h) {
+    float aw = (float)im.w / w;
+    float ah = (float)im.h / h;
+    float bw = 0.5 * (aw - 1);
+    float bh = 0.5 * (ah - 1);
+
+    image img = make_image(w, h, im.c);
+    for (int i = 0; i < w; i++) {
+      for (int j = 0; j < h; j++) {
+        for (int k = 0; k < im.c; k++) {
+          float val = nn_interpolate(im, aw * i + bw, ah * j + bh, k);
+          set_pixel(img, i, j, k, val);
+        }
+      }
+    }
+    return img;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
